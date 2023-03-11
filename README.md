2ad275e3215df060359eed5e8a7d977ec09487c4
akhilmondal <akhilmandalfamily@gmail.com>
2023-03-11 19:20:00 +0530

    Added: Program to show welcome message in master branch.

---
+++ README.md
@@ -0,0 +1,1 @@
+# FindMaximumDay13PracticeProblem
---
+++ src/com/generics/findmax/FindMaximum.java
@@ -0,0 +1,9 @@
+package com.generics.findmax;
+
+public class FindMaximum {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Test max Number using Generics!");
+    }
+}
